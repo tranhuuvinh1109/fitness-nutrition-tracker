10ddb4b2b293b5e6705bff55ebe341759ae086db
VinhTranHuu <vinh.tranhuu@ncc.asia>
2026-01-10 21:28:10 +0700

    update: schema

--- app/schemas/workout_suggestion_schema.py
+++ app/schemas/workout_suggestion_schema.py
@@ -2,30 +2,36 @@ from marshmallow import Schema, fields
 
 
 class WorkoutSuggestionWorkoutInfoSchema(Schema):
-    id = fields.Str()
-    name = fields.Str()
-    type = fields.Str()
-    met = fields.Float(allow_none=True)
+    id = fields.Str(dump_only=True)
+    name = fields.Str(dump_only=True)
+    type = fields.Method("get_type", dump_only=True)
+    met = fields.Float(allow_none=True, dump_only=True)
+    
+    def get_type(self, obj):
+        """Convert enum to string"""
+        if hasattr(obj.type, 'value'):
+            return obj.type.value
+        return str(obj.type) if obj.type else None
 
 
 class WorkoutSuggestionLogInfoSchema(Schema):
-    id = fields.Str()
-    user_id = fields.Str()
-    workout_id = fields.Str()
-    duration_min = fields.Int()
-    calories_burned = fields.Int(allow_none=True)
-    log_date = fields.Date()
-    created_at = fields.DateTime()
+    id = fields.Str(dump_only=True)
+    user_id = fields.Str(dump_only=True)
+    workout_id = fields.Str(dump_only=True)
+    duration_min = fields.Int(dump_only=True)
+    calories_burned = fields.Int(allow_none=True, dump_only=True)
+    log_date = fields.Date(dump_only=True)
+    created_at = fields.DateTime(dump_only=True)
 
 
 class WorkoutSuggestionItemSchema(Schema):
-    workout = fields.Nested(WorkoutSuggestionWorkoutInfoSchema)
-    log = fields.Nested(WorkoutSuggestionLogInfoSchema, allow_none=True)
-    description = fields.Str()
-    day_of_week = fields.Int()
+    workout = fields.Nested(WorkoutSuggestionWorkoutInfoSchema, dump_only=True)
+    log = fields.Nested(WorkoutSuggestionLogInfoSchema, allow_none=True, dump_only=True)
+    description = fields.Str(dump_only=True)
+    day_of_week = fields.Int(dump_only=True)
 
 
 class WorkoutSuggestionResponseSchema(Schema):
-    sessions_per_week = fields.Int()
-    start_date = fields.Str()
-    workouts = fields.List(fields.Nested(WorkoutSuggestionItemSchema))
+    sessions_per_week = fields.Int(dump_only=True)
+    start_date = fields.Str(dump_only=True)
+    workouts = fields.List(fields.Nested(WorkoutSuggestionItemSchema), dump_only=True)
